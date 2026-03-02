bec5e0675498338e901a674b7b0878eb09e22a48
doaaelomda <delomda8@gmail.com>
2026-03-02 21:29:37 +0200

    HANDLE endpoints and slider icons

--- apps/angular-c4-team3/src/app/features/prodacts/pages/product-related/product-related.component.ts
+++ apps/angular-c4-team3/src/app/features/prodacts/pages/product-related/product-related.component.ts
@@ -4,10 +4,12 @@ import { TranslocoModule } from '@jsverse/transloco';
 import { SliderComponent } from 'apps/angular-c4-team3/src/app/shared/components/slider/slider.component';
 import { ProductsService } from '../../services/prodacts.service';
 import { RelatedProduct, RelatedProductsResponse } from '../../related-product';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { StyledHomeTitleComponent } from 'apps/angular-c4-team3/src/app/shared/components/styled-home-title/styled-home-title.component';
 
 @Component({
   selector: 'app-product-related',
-  imports: [TranslocoModule, SliderComponent],
+  imports: [TranslocoModule, SliderComponent,StyledHomeTitleComponent],
   templateUrl: './product-related.component.html',
   styleUrl: './product-related.component.css',
 })
@@ -24,7 +26,7 @@ export class ProductRelatedComponent implements OnInit {
   getRelatedProdacts(id: string) {
     this.ProductsService.getRelatedProdacts(id).subscribe((res: RelatedProductsResponse) => {
       this.products = res.relatedProducts;
-      this.cdr.detectChanges();   // 👈 الحل هنا
+      this.cdr.detectChanges();
     });
   }
 }
