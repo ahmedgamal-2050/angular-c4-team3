2985d46713180887e52e96f62270645c99578e1d
doaaelomda <delomda8@gmail.com>
2026-03-08 21:53:59 +0200

    delete unused code

--- apps/angular-c4-team3/src/app/shared/components/slider/slider.component.ts
+++ apps/angular-c4-team3/src/app/shared/components/slider/slider.component.ts
@@ -4,7 +4,6 @@ import { LucideAngularModule, ShoppingCart, Heart } from 'lucide-angular';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { ICONS_SVG } from '../../constants/icons-svg';
 import { TranslocoModule } from '@jsverse/transloco';
-import { RouterLink } from "@angular/router";
 import { APP_ROUTES } from '../../constants/app-routes';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { Product } from '../../../features/landing/pages/home/home.model';
@@ -12,7 +11,7 @@ import { Product } from '../../../features/landing/pages/home/home.model';
 @Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [CarouselModule, LucideAngularModule, TranslocoModule, RouterLink,ProductCardComponent],
+  imports: [CarouselModule, LucideAngularModule, TranslocoModule,ProductCardComponent],
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.css'],
 })
@@ -21,34 +20,10 @@ export class SliderComponent {
   products  = input<Product[]>([]);
   numVisible = input(4);
 
-  readonly Heart = Heart;
-  readonly ShoppingCart = ShoppingCart;
-  readonly APP_ROUTES = APP_ROUTES;
-
   responsiveOptions: CarouselResponsiveOptions[] = [
     { breakpoint: '1280px', numVisible: 3, numScroll: 1 },
     { breakpoint: '1024px', numVisible: 2, numScroll: 1 },
     { breakpoint: '640px', numVisible: 1, numScroll: 1 },
   ];
-  private sanitizer = inject(DomSanitizer);
 
-  getDiscountPercent(price: number, priceAfterDiscount: number): number {
-    return Math.round(((price - priceAfterDiscount) / price) * 100);
-  }
-
-  getStarsArray(rating: number): SafeHtml[] {
-    const stars: SafeHtml[] = [];
-    const fullStars = Math.floor(rating);
-    const hasHalfStar = rating % 1 !== 0;
-
-    for (let i = 0; i < 5; i++) {
-      if (i < fullStars) {
-        stars.push(this.sanitizer.bypassSecurityTrustHtml(ICONS_SVG.fullStar));
-      } else if (i === fullStars && hasHalfStar) {
-        stars.push(this.sanitizer.bypassSecurityTrustHtml(ICONS_SVG.halfStar));
-      }
-    }
-
-    return stars;
-  }
 }
