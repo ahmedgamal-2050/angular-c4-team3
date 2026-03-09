565bccbf2ef1d5ec0b13d4c48b3ad4cc9db2bef9
ahmed gamal <ahmed.gamal@areebgroup.com>
2026-03-09 09:38:21 +0200

    fix: Conditionally adjust Transloco asset path for development and production environments.

--- apps/angular-c4-team3/src/app/transloco-loader.ts
+++ apps/angular-c4-team3/src/app/transloco-loader.ts
@@ -1,12 +1,16 @@
 import { inject, Injectable } from '@angular/core';
 import { Translation, TranslocoLoader } from '@jsverse/transloco';
 import { HttpClient } from '@angular/common/http';
+import { isDevMode } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
   private http = inject(HttpClient);
 
   getTranslation(lang: string) {
-    return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
+    const url = isDevMode()
+      ? `/assets/i18n/${lang}.json`
+      : `/angular-c4-team3/assets/i18n/${lang}.json`;
+    return this.http.get<Translation>(url);
   }
 }
