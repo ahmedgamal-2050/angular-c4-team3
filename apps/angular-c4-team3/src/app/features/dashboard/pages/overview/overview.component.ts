60ed1feb4d61bc8ca83b6aefe717b8c0b2f0ac77
ahmed.gamal <eng.ahmedgamal2050@gmail.com>
2026-06-06 17:02:44 +0300

    feat: add overview overall section + integrate with all statistics api

--- apps/angular-c4-team3/src/app/features/dashboard/pages/overview/overview.component.ts
+++ apps/angular-c4-team3/src/app/features/dashboard/pages/overview/overview.component.ts
@@ -1,8 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { OverviewService } from './services/overview.service';
+import {
+  AllStatisticsResponse,
+  Category,
+  LyRevenue,
+  OrdersByStatus,
+  Overall,
+  Product,
+  ProductsByCategory,
+} from './overview.model';
+import { OverviewOverallSectionComponent } from './components/overview-overall-section/overview-overall-section.component';
 
 @Component({
   selector: 'app-overview',
-  imports: [],
+  imports: [OverviewOverallSectionComponent],
   templateUrl: './overview.component.html',
 })
-export class OverviewComponent {}
+export class OverviewComponent implements OnInit {
+  readonly overviewService = inject(OverviewService);
+
+  overall = signal<Overall | null>(null);
+  categories = signal<Category[]>([]);
+  lowStockProducts = signal<Product[]>([]);
+  topSellingProducts = signal<Product[]>([]);
+  productsByCategory = signal<ProductsByCategory[]>([]);
+  ordersByStatus = signal<OrdersByStatus[]>([]);
+  dailyRevenue = signal<LyRevenue[]>([]);
+  monthlyRevenue = signal<LyRevenue[]>([]);
+
+  ngOnInit(): void {
+    this.getAllStatistics();
+  }
+
+  getAllStatistics() {
+    this.overviewService.getAllStatistics().subscribe({
+      next: (res: AllStatisticsResponse) => {
+        this.overall.set(res.statistics.overall);
+        this.categories.set(res.statistics.categories);
+        this.lowStockProducts.set(res.statistics.products.lowStockProducts);
+        this.topSellingProducts.set(res.statistics.products.topSellingProducts);
+        this.productsByCategory.set(res.statistics.products.productsByCategory);
+        this.ordersByStatus.set(res.statistics.orders.ordersByStatus);
+        this.dailyRevenue.set(res.statistics.orders.dailyRevenue);
+        this.monthlyRevenue.set(res.statistics.orders.monthlyRevenue);
+      },
+    });
+  }
+}
