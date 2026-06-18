595e0ed649c848ef9d3e525e7c4f4df08c2ea7d2
ahmed.gamal <eng.ahmedgamal2050@gmail.com>
2026-06-18 19:15:13 +0300

    feat: add dashboard top navbar

--- apps/dashboard/src/app/layout/dashboard-wrapper/dashboard-wrapper.component.ts
+++ apps/dashboard/src/app/layout/dashboard-wrapper/dashboard-wrapper.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
-import { BreadcrumbComponent } from '../../shared/components/breadcrumb/breadcrumb.component';
 import { BottomNavbarComponent } from '../../shared/components/bottom-navbar/bottom-navbar.component';
+import { DashboardTopNavbarComponent } from '../../shared/components/dashboard-top-navbar/dashboard-top-navbar.component';
 
 @Component({
   selector: 'app-dashboard-wrapper',
   imports: [
     RouterOutlet,
     SidebarComponent,
-    BreadcrumbComponent,
     BottomNavbarComponent,
+    DashboardTopNavbarComponent,
   ],
   templateUrl: './dashboard-wrapper.component.html',
 })
